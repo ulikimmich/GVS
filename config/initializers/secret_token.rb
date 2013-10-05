db4b9c14c4a142578d7c0b2e3ddb0dae306546ee
Uli Kimmich <uli@globalvirtualstudio.com>
2013-10-04 15:32:06 -1000

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GVS::Application.config.secret_key_base = '4d9fcd1e9401e90c45af0cfaa0b234a46acd8920fe405c44bad31254ab281b50fb7db4f05425fe69fa9d606f134e9b6840b4a759b5c3f7952ce01d806d8b6a58'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GVS::Application.config.session_store :cookie_store, key: '_GVS_session'
