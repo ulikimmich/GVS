9624a71d99e9bfebb694fbd1515a37f127bfae72
Uli Kimmich <uli@globalvirtualstudio.com>
2013-10-08 13:37:07 -1000

    added partials

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module GVS
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
   end
