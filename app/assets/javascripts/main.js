031e6f2b51f615718128117ee8342b4ec962bbe2
Uli Kimmich <uli@globalvirtualstudio.com>
2013-10-21 15:26:02 -1000

    added styling and new GVS logo

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,4 @@
 // You need to specify the size of your background image here (could be done automatically by some PHP code)
-var FullscreenrOptions = {  width: 1900, height: 1342, bgID: '#bgimg' };
+var FullscreenrOptions = {  width: 1200, height: 796, bgID: '#bgimg' };
 // This will activate the full screen background!
 jQuery.fn.fullscreenr(FullscreenrOptions);
