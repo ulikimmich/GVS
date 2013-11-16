637684bdeb8ac6e0acd4555bd6986fa34c3e359a
Uli Kimmich <uli@globalvirtualstudio.com>
2013-11-15 14:00:39 -1000

    user_mailer To address changed to Felipe's

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
-  default from: "no-reply@globalvirtualstudio.com"
-  default to: "noreply@globalvirtualstudio.com"
+  default from: "noreply@globalvirtualstudio.com"
+  default to: "f.zamora@globalvirtualstudio.com"
 
   attr_accessor :name, :email, :body
 
