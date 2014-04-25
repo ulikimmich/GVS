ad027780c04d234c9e1f7c3374c65f51824e0302
Uli Kimmich <uli@globalvirtualstudio.com>
2014-04-25 08:15:30 -1000

    added press links, changed session duration to 1 day

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,8 @@ module SessionsHelper
 
   def sign_in(user)
     remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
+    cookies[:remember_token] = { value: remember_token, expires: 1.day.from_now }
+    #cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
   end
